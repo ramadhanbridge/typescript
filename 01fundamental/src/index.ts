60638cab88f6af94161f91d672f7582e2806f364
ramadhanbridge <nyiringondoramadan@gmail.com>
2022-06-18 22:29:28 +0200

    turples

--- 01fundamental/src/index.ts
+++ 01fundamental/src/index.ts
@@ -8,5 +8,10 @@ function render(document: any) {
     console.log(document)
 }
 
+//arrays
 
-let numbers: number[] = [1, 2, 3, '4']
+let numbers: number[] = [1, 2, 3]
+
+// tuples
+
+let user: [number, string] = [1, 'mosh']
